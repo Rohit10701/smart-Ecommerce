b62d8eb0878ac621b437d4e3d4745a6f52a34751
Rohit10701 <rohitkumar10701@gmail.com>
2023-10-12 06:01:04 +0530

    bug fixed 2.2

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,9 +1,9 @@
 import { productData } from "@/constants/data";
 
 export const getProducts = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`);
-  if (!res.ok){
-    throw new Error("Failed to fetch Products");
+  const res = await fetch("https://fakestoreapiserver.reactbd.com/smart");
+  if (!res.ok) {
+    throw new Error("Faild to fetch products");
   }
   return res.json();
 };
